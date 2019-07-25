983bd3ef4f1b78f191be0cd5640da47dd4d5ea07
Jaroslav Malik <jaroslav.malik@gmail.com>
2019-07-25 20:26:43 +0200

    Roles controller

---
+++ src/RestController/RoleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\RestController;
+
+use App\Entity\Role;
+use App\DTO\RoleDTO;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View; 
+
+class RoleController extends FOSRestController
+{
+    /**
+     * Retrieves a collection of Users resource
+     * @Rest\Get("/roles")
+     * @return View
+     */
+    public function getRolesX(): View
+    {
+        $roles = $this->getDoctrine()->getRepository(Role::class)->findAll();
+		$rolesDTO = array();
+		foreach ($roles as $role) {
+			$rolesDTO[$role->getId()] = new RoleDTO(
+				$role->getId(), 
+				$role->getCode(), 
+				$role->getName()
+			);
+		}
+        // In case our GET was a success we need to return a 200 HTTP OK response with the collection of article object
+        return View::create($rolesDTO, Response::HTTP_OK);
+    }
+}
